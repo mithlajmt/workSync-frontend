681eaa99d1d3c6b7b6bb2420894b1cc8cede84ca
mithlajmatta <144442116+mithlajmatta@users.noreply.github.com>
2024-02-12 10:14:09 +0530

    login modified

--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+// login.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      rememberMe: [false,]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      // Handle form submission logic here
+      console.log('Form submitted:', this.loginForm.value);
+    }
+  }
 }
